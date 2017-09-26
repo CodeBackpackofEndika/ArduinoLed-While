633ce2c16119b3713c3328c60d24fb8f2f46ed7d
CodeBackpackofEndika <32270483+CodeBackpackofEndika@users.noreply.github.com>
2017-09-26 13:49:41 +0200

    Add files via upload

---
+++ ArduinoLed-While.ino
@@ -0,0 +1,19 @@
+byte Led1=12; 
+byte cont=0; 
+byte tiempo=500; 
+ 
+void setup() { 
+pinMode(Led1,OUTPUT); 
+digitalWrite(Led1,LOW); 
+ 
+} 
+ 
+void loop() { 
+while(cont<10){ 
+  digitalWrite(Led1,HIGH); 
+  delay(tiempo); 
+  digitalWrite(Led1,LOW); 
+  delay(tiempo); 
+  cont=cont+1; 
+} 
+} 
